da820fd08cb4c0b7847b3c292d0678ef4cf4e316
Ejaz Abbas <140273530+ejazabbas73@users.noreply.github.com>
2023-07-23 01:46:47 +0530

    Update imd_top_250_data_scraper.py

--- imd_top_250_data_scraper.py
+++ imd_top_250_data_scraper.py
@@ -23,25 +23,6 @@ def Top250Movies(url):
 
     return titles, ratings
 
-def Top250Movies(url):
-    titles = []
-    ratings = []
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-    with requests.Session() as session:
-        req = session.get(url, headers=headers)
-        req.raise_for_status() 
-        content = BeautifulSoup(req.content, 'html.parser')
-
-    data = content.find_all('li', {'class': 'ipc-metadata-list-summary-item'})
-    for i in data:
-        ti  = i.find('h3', attrs={'class': 'ipc-title__text'})
-        ra  = i.find('span', attrs={'class': 'ipc-rating-star ipc-rating-star--base ipc-rating-star--imdb ratingGroup--imdb-rating'})
-        titles.append(ti.text.strip(f"{digits}."))
-        ratings.append(ra.text.strip())
-
-    return titles, ratings
-
 start_time = time.time()
 url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
 titles, ratings = Top250Movies(url)
